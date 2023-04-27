bfe6a69e33eb18cff8cd0cd67a5d2bfad486075c
csric <richcsr256@gmail.com>
2023-04-27 15:20:38 +0800

    nothing

--- applications/Chat/examples/ray/benchmark.sh
+++ applications/Chat/examples/ray/benchmark.sh
@@ -1,9 +1,10 @@
-#  "facebook/opt-2.7b" 
+
 PROMPT_PATH=/home/lccsr/data3/awesome-chatgpt-prompts/prompts.csv
 
 num_trainers=4
 num_makers=4
 
+#  "facebook/opt-2.7b" 
 for pretrain in "facebook/opt-1.3b" "facebook/opt-6.7b" "facebook/opt-13b"
 do
     
@@ -16,7 +17,8 @@ do
                 # set a big enough experience_steps for twice maker-update
                 experience_steps=$((2*num_trainers*train_batch_size*update_steps/num_makers/experience_batch_size))
 
-                echo running: ${num_trainers}_${num_makers}_pretrain_${pretrain##*/}_experience_batch_size_${experience_batch_size}_train_batch_size_${train_batch_size}_update_steps_${update_steps}_experience_steps_${experience_steps}
+                config_string=${num_trainers}_${num_makers}_pretrain_${pretrain##*/}_experience_batch_size_${experience_batch_size}_train_batch_size_${train_batch_size}_update_steps_${update_steps}_experience_steps_${experience_steps}
+                echo running: ${config_string}
 
                 nohup python mmmt_prompt.py \
                     --prompt_path $PROMPT_PATH \
@@ -30,7 +32,7 @@ do
                     --experience_batch_size $experience_batch_size \
                     --update_steps $update_steps \
                     --train_batch_size $train_batch_size \
-                    --debug > logs/output_${num_trainers}_${num_makers}_pretrain_${pretrain##*/}_experience_batch_size_${experience_batch_size}_train_batch_size_${train_batch_size}_update_steps_${update_steps}_experience_steps_${experience_steps}.txt 2>&1
+                    --debug > logs/output_${config_string}.txt 2>&1
             done
         done
     done
